9b46baa896acca298371c9ebfc48698495b389e4
risdian <dxlift@gmail.com>
2024-02-02 09:50:30 +0800

    new

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-print(os.getenv("DB_NAME"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,13 +75,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+        "ENGINE": os.getenv('DB_ENGINE'),
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),  # set in docker-compose.yml
+        "PORT": os.getenv('DB_PORT'),  # default postgres port
     }
+    
 }
 
 
