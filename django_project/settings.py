e83314776bdb14f679b24d20e04a487203088abd
risdian <dxlift@gmail.com>
2024-02-05 15:43:23 +0800

    COMMIT

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,17 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz%q*vf(8vnvpb!i5pf(13!wom1#imf3le4^uk(w!z_hpp1_%q'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,14 +76,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv('DB_ENGINE'),
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),  # set in docker-compose.yml
-        "PORT": os.getenv('DB_PORT'),  # default postgres port
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.getenv("DB_NAME", "default_db_name"),
+        "USER": os.getenv("DB_USER", "default_db_user"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "default_db_password"),
+        "HOST": os.getenv("DB_HOST", "localhost"),  # Default to localhost if not specified
+        "PORT": os.getenv("DB_PORT", 5432),  # Default PostgreSQL port
     }
-    
 }
 
 
